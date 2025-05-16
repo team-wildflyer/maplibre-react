7c4f13dbc610a5888fa3b4b52e5b5a109fd358cf
Joost Lubach <joostlubach@gmail.com>
2025-05-16 15:36:29 +0200

    Fix in groups

--- src/layers/LayerGroup.tsx
+++ src/layers/LayerGroup.tsx
@@ -1,9 +1,10 @@
 import { omit } from 'lodash'
-import React, { ReactNode, useEffect } from 'react'
+import React, { ReactNode, useEffect, useMemo } from 'react'
 import { memo } from 'react-util'
 import { useWithStableDeps } from 'react-util/hooks'
 import { useMap } from '../MapContext'
 import { LayerGroupOrdering } from '../types'
+import { LayerGroupContext } from './LayerGroupContext'
 
 export type LayerGroupProps = LayerGroupCommonProps & LayerGroupOrdering
 
@@ -21,12 +22,20 @@ export const LayerGroup = memo('LayerGroup', (props: LayerGroupProps) => {
     ordering => ['above' in ordering ? `above-${ordering.above}` : `below-${ordering.below}`]
   )
 
+  const contextValue: LayerGroupContext = useMemo(() => ({
+    name,
+  }), [name])
+
   const map = useMap()
   useEffect(
     () => map.registerLayerGroup(name, positioning),
     [map, name, positioning]
   )
 
-  return <>{children}</>
+  return (
+    <LayerGroupContext.Provider value={contextValue}>
+      {children}
+    </LayerGroupContext.Provider>
+  )
 
 })
